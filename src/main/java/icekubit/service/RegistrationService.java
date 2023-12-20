b9e88e5a7f9a928e56023152c8b1e1d5dea0835b
icekubit <myznikovkirill@gmail.com>
2023-12-20 16:38:58 +0300

    refactor: remove check if user already exist from RegistrationService and make it depending on case-insensitive unique login constraint in database

--- src/main/java/icekubit/service/RegistrationService.java
+++ src/main/java/icekubit/service/RegistrationService.java
@@ -18,10 +18,6 @@ public class RegistrationService {
         User user = new User();
         user.setLogin(username);
         user.setPassword(PasswordUtil.hashPassword(password));
-        // check if user exists - can't rely on case-sensitive username unique constraint
-        if (userDao.findByLogin(username).isPresent()) {
-            throw new UserAlreadyExistException("This user already exists");
-        }
         userDao.save(user);
     }
 
