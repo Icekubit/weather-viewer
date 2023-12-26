c206c916ec721eaf1b0a28d71a771099fb7fb7d3
icekubit <myznikovkirill@gmail.com>
2023-12-26 11:12:39 +0300

    refactor: delete unused dependencies and imports from DeleteLocationServlet

--- src/main/java/icekubit/servlet/DeleteLocationServlet.java
+++ src/main/java/icekubit/servlet/DeleteLocationServlet.java
@@ -2,33 +2,25 @@ package icekubit.servlet;
 
 import icekubit.entity.Location;
 import icekubit.entity.User;
-import icekubit.exception.UnauthorizedActionException;
 import icekubit.service.UserWeatherService;
-import icekubit.util.ThymeleafUtil;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
 
 import java.io.IOException;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @WebServlet("/delete_location")
 public class DeleteLocationServlet extends BaseServlet {
     private UserWeatherService userWeatherService;
-    private TemplateEngine templateEngine;
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         ServletContext servletContext = config.getServletContext();
         userWeatherService = (UserWeatherService) servletContext.getAttribute("userWeatherService");
-        templateEngine = (TemplateEngine) servletContext.getAttribute("templateEngine");
-
     }
 
     @Override
